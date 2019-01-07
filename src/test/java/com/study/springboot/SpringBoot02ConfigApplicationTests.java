ab9f6be70004b1e8ca9d22b331c035ac687921a4
wangxingang4 <wangxingang@jd.com>
2019-01-07 16:58:55 +0800

    yml

--- src/test/java/com/study/springboot/SpringBoot02ConfigApplicationTests.java
+++ src/test/java/com/study/springboot/SpringBoot02ConfigApplicationTests.java
@@ -1,7 +1,9 @@
 package com.study.springboot;
 
+import com.study.springboot.domain.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBoot02ConfigApplicationTests {
 
+    @Autowired
+    Person person;
+
     @Test
     public void contextLoads() {
+        System.out.println("\n");
+        System.out.println(person);
+        System.out.println("\n");
     }
 
 }
